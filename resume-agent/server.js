62900cba96e78b954afcfc60fe98e85eb6f4b65a
Gaganharikiranmarella <ghk7125@gmail.com>
2026-09-15 15:30:46 +0530

    server changes

--- resume-agent/server.js
+++ resume-agent/server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+const express = require('express');
 const { screenResume, validateCandidate, matchToRole, draftInterviewInvite } = require('./lib/pipeline');
 const { listRoles, getRole } = require('./lib/jobRoles');
 const { sendInterviewNotice, isConfigured, SENDER } = require('./lib/mailer');
